935f62348a9d5dfd69dd0e24845aefe06cfd0478
Greg <c_o_t_c_o_s@hotmail.com>
2018-10-18 19:46:31 +0100

    Started game flow screens and defined basic layout

---
+++ False Flags/Assets/Scripts/RoundData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoundData {
+
+    public string name;
+    public int timeLimitInSeconds;          //Difficulty modifier?
+    public int pointsAddedForCorrectAnswer; 
+    public QuestionData[] questions;        //Testing purposes
+}
