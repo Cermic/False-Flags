7ad751a6e8fbe2edd34cb05a6643765907112dbc
Greg <sephiroth198@googlemail.com>
2018-11-04 18:54:10 +0000

    Vid
    Initial setup completed

--- False Flags/Assets/Scripts/Managers/GameController.cs
+++ False Flags/Assets/Scripts/Managers/GameController.cs
@@ -1,14 +1,126 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public Text questionDisplayText;
+    public Text scoreDisplayText;
+    public Text timeRemaningDisplayText;
+    public SimpleObjectPool answerButtonObjectPool;
+    public Transform answerButtonParent;
+    public GameObject questionDisplay, roundEndDisplay;
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+    private bool isRoundActive;
+    private float timeRemaining;
+    private int questionIndex;
+    private int playerScore;
+
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+    public Scrollbar progressBar;
+
+
+    // Use this for initialization
+    void Start() {
+        dataController = FindObjectOfType<DataController>();
+        currentRoundData = dataController.GetCurrentRoundData();
+        questionPool = currentRoundData.questions;
+        timeRemaining = currentRoundData.timeLimitInSeconds;
+        UpdateTimeRemaningDisplay();
+
+        playerScore = 0;
+        questionIndex = 0;
+
+        ShowQuestion();
+        isRoundActive = true;
+
+    }
+
+    /// <summary>
+    /// ShowQuestion and RemoveAnswerButtons are parts of the non canvas setup for the question buttons
+    /// This works by creating a button prefab that forms into a vertical list of buttons
+    /// May need to switch to non-canvas version used in video or look into modifying this section to change pre-defined buttons in the canvas.
+    /// </summary>
+    private void ShowQuestion()
+    {
+        RemoveAnswerButtons();
+        QuestionData questionData = questionPool[questionIndex];
+        questionDisplayText.text = questionData.questionText;
+
+        for(int i= 0;i< questionData.answers.Length; i++)
+        {
+            GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
+            answerButtonGameObjects.Add(answerButtonGameObject);
+            answerButtonGameObject.transform.SetParent(answerButtonParent);
+
+            AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+            answerButton.Setup(questionData.answers[i]);
+
+        }
+    }
+
+    private void RemoveAnswerButtons() {
+        while (answerButtonGameObjects.Count > 0)
+        {
+            answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+        }
+    }
+
+    public void AnswerButtonClicked (bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            playerScore += currentRoundData.pointsAddedForCorrectAnswer;
+            scoreDisplayText.text = "Score: " + playerScore.ToString();
+        }
+
+        if (questionPool.Length > questionIndex + 1)
+        {
+            questionIndex++;
+            ShowQuestion();
+        }
+        else
+        {
+            EndRound();
+        }
+    }
+
+    public void EndRound()
+    {
+        isRoundActive = false;
+        questionDisplay.SetActive (false);
+        roundEndDisplay.SetActive (true);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    private void UpdateTimeRemaningDisplay()
+    {
+        string temp = (Mathf.Round(timeRemaining)).ToString();
+        timeRemaningDisplayText.text = "Time: " + temp;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (isRoundActive)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimeRemaningDisplay();
+
+            if (timeRemaining <= 0.0f)
+            {
+                EndRound();
+            }
+        }
 	}
 }
