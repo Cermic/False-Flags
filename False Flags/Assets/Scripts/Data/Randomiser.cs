7de7039d90a209bcaf5462c2f06cc28d70130c24
Greg <sephiroth198@googlemail.com>
2018-11-10 00:54:50 +0000

    Randomisation
    Random generator implemented.
    Answers seleted at random.
    Correct answer also set by randomiser.
    Setup JSON for flag data

---
+++ False Flags/Assets/Scripts/Data/Randomiser.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomiser : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    public RoundData RandomGeneration(RoundData[] allRoundData, FlagData[] flagData)
+    {
+        for (int i = 0; i < allRoundData[0].questions.Length; i++)
+        {
+
+            for (int j = 0; j < 4; j++)
+            {
+                allRoundData[0].questions[i].answers[j] = flagData[0].answers[RandomNumber(flagData[0].answers.Length)];
+            }
+
+            //Checks if answers generated are unique values
+            for (int k = 0; k < 4; k++)
+            {
+                for (int m = 0; m < 4; m++)
+                {
+                    //Compares all values to each other value
+                    if (k != m)
+                    {
+                        while (allRoundData[0].questions[i].answers[k] == allRoundData[0].questions[i].answers[m])
+                        {
+                            allRoundData[0].questions[i].answers[k] = flagData[0].answers[RandomNumber(flagData[0].answers.Length)];
+                        }
+                    }
+                }
+            }
+
+            allRoundData[0].questions[i].answers[RandomNumber(3)].isCorrect = true;
+        }
+        return allRoundData[0];
+    }
+
+    private int RandomNumber(int num)
+    {
+        int value = (int)(Random.Range(0.0f, (float)num));
+        return value;
+    }
+}
